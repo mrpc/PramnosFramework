8f5f4b73fdc8f039b1981e9356b652090d94cca6
Yannis - Pastis Glaros <mrpc@pramnoshosting.gr>
2015-08-06 03:43:43 +0300

    The first tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,3 +1,31 @@
 <?php
-require __DIR__.'/vendor/autoload.php';
+//Include composer loader
+require dirname(__DIR__) . DIRECTORY_SEPARATOR
+    . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 date_default_timezone_set('UTC');
+/**
+* The following are REQUIRED by Pramnos Framework
+*/
+
+/**
+* Define Paths if paths are not defined.
+* It auto-defines based on where this file is placed.
+*/
+if (!defined('URL')) {
+    define('URL', ''); //URL (in case of secondary applications)
+}
+if (!defined('sURL')) {
+    define('sURL', ''); //URL (in case of secondary applications)
+}
+if (!defined('ROOT')) {
+    define('ROOT', dirname(dirname(__FILE__)));
+}
+if (!defined('DS')) {
+    define('DS', DIRECTORY_SEPARATOR);
+}
+if (!defined('SP')) {
+    define('SP', 1); //Start point - to avoid running files without one.
+}
+/**
+* EOF REQUIRED DEFINES
+*/
