e1aa6f114dea4b44049948bf46de42a129e55185
mrpc <mrpc@pramnoshosting.gr>
2020-02-04 10:53:13 +0200

    Application is now working

--- src/Pramnos/Application/Application.php
+++ src/Pramnos/Application/Application.php
@@ -15,6 +15,7 @@ class Application extends Base
      * @var mixed
      */
     public $activeController = null;
+
     public $currentUser = null;
     /**
      * Main Application Information
@@ -41,26 +42,59 @@ class Application extends Base
      * @var Settings
      */
     public $settings;
+    /**
+     * Database object
+     * @var \Pramnos\Database\Database
+     */
     public $database;
-    public $_defaultModule;
-    public $module = '';
+    /**
+     * Default controller to run. Defaults to "home"
+     * @var string
+     */
+    public $defaultController = 'home';
+    /**
+     * Current controller name
+     * @var string
+     */
+    public $controller = '';
+    /**
+     * Current action to run
+     * @var string
+     */
     public $action;
-    private $_moduleinfo = array(
-        "moduletype" => '',
+    /**
+     * Controller information
+     * @var string
+     */
+    private $controllerInfo = array(
+        "type" => '',
         "title" => '',
         "id" => null
     );
     private $_isStartPage = true;
-    private $_redirect = null;
-
     /**
-     * Extra paths to look when getting models or views
-     * @var array
+     * Redirect address
+     * @var string
      */
-    private $_extraPath = array();
+    private $_redirect = null;
+    /**
+     * Did the application already initialize?
+     * @var bool
+     */
+    protected $initialized = false;
+    /**
+     * Application instances
+     * @var Application[]
+     */
+    protected static $appInstances = array();
+    /**
+     * Last used application name
+     * @var string
+     */
+    protected static $lastUsedApplication = null;
 
     /**
-     *
+     * Application class constructor
      * @param string $appName Application Name used for namespaces
      */
     public function __construct($appName = '')
@@ -74,8 +108,24 @@ class Application extends Base
         } else {
             $this->applicationInfo = require APP_PATH . DS . $appName . '.php';
         }
+        if (!defined('URL')) {
+            define('URL', getUrl());
+        }
+        if (!defined('sURL')) {
+            if ($appName == '') {
+                define('sURL', URL);
+            } else {
+                define('sURL', basename(URL));
+            }
+        }
 
         parent::__construct();
+        if ($appName == '') {
+            self::$appInstances['default'] = $this;
+        } else {
+            self::$appInstances[$appName] = $this;
+        }
+        self::$lastUsedApplication = $appName;
     }
 
     /**
@@ -107,6 +157,9 @@ class Application extends Base
         if (!defined('ADDONS_PATH')) {
             define('ADDONS_PATH', APP_PATH . DS . 'addons');
         }
+        if (!defined('LOG_PATH')) {
+            define('LOG_PATH', VAR_PATH);
+        }
         define('PRAMNOS_DEFINES', true);
     }
 
@@ -115,19 +168,43 @@ class Application extends Base
      */
     public function init($settingsFile = '')
     {
+        if ($this->initialized === true) {
+            return;
+        }
         $this->settings = Settings::getInstance($settingsFile);
-        $this->database = new \Pramnos\Database\Database($this->settings);
+        $this->database = \Pramnos\Database\Database::getInstance(
+            $this->settings
+        );
         try {
             $this->database->connect();
         } catch (Exception $ex) {
             die($ex->getMessage());
         }
-        
+        $this->initialized = true;
         /**
          * Start Session
          */
         $this->session = \Pramnos\Http\Session::getInstance();
         $this->session->start();
+
+
+        $request = new \Pramnos\Http\Request();
+        if ($request->getController() !== '') {
+            $this->controller = $request->getController();
+        }
+        $this->action = $request->getAction();
+        if (isset($_SESSION['language'])) {
+            $this->language = $_SESSION['language'];
+        }
+        //End of set session defaults
+        if (isset($_GET['lang']) == true) {
+            $_SESSION['language'] = $_GET['lang'];
+            $this->language = $_GET['lang'];
+        }
+        $lang = \Pramnos\Translator\Language::getInstance($this->language);
+        $lang->load($this->language);
+        \Pramnos\Addon\Addon::triger('AppInit', 'system');
+
     }
 
     /**
@@ -145,14 +222,72 @@ class Application extends Base
      * @param boolean $quit If you want to quit after redirecting.
      * @param string  $code Forces HTTP response code to the specified value.
      */
-    public function redirect($url = null, $quit = true, $code='301')
+    public function redirect($url = null, $quit = true, $code='302')
     {
+        //@codeCoverageIgnoreStart
+        if (defined('DEVELOPMENT') && DEVELOPMENT && $url != '') {
+            $backtrace = debug_backtrace();
+            $back = '';
+            $comma = " - ";
+            foreach ($backtrace as $backTraceObject) {
+                if (isset($backTraceObject['file'])
+                    && isset($backTraceObject['line'])) {
+                    $back .= $comma
+                        . $backTraceObject['file']
+                        . " :: "
+                        . $backTraceObject['line'];
+                    $comma = "\n - ";
+                }
+            }
 
+            $request = new \Pramnos\Http\Request();
+            \Pramnos\Logs\Logs::log(
+                "\n"
+                . 'Redirect from: '
+                . $request->getURL(false)
+                . ' to '
+                .
+                $url .
+                "\nBacktrace:\n"
+                . $back,
+                'redirects'
+            );
+        }
+        //@codeCoverageIgnoreEnd
+        if ($url !== null) {
+            if (!headers_sent()) {
+                header("Location: " . $url, true, $code);
+            }
+            echo '<script>window.location="'
+                . $url
+                . '"</script>Redirecting. If your browser doesn\'t '
+                . 'redirect, please click '
+                . '<a href="' . $url . '">here</a>.';
+            if ($quit == true) {
+                $this->close();
+            }
+            return true;
+        }
+        if ($this->_redirect !== null) {
+            if (!headers_sent()) {
+                header("Location: " . $this->_redirect, true, $code);
+            }
+            echo '<script>window.location="'
+                . $this->_redirect
+                . '"</script>Redirecting. If your browser doesn\'t '
+                . 'redirect, please click '
+                . '<a href="' . $this->_redirect . '">here</a>.';
+            if ($quit == true) {
+                $this->close();
+            }
+            return true;
+        }
+        return false;
     }
 
-    public function setmoduleinfo($moduleinfo = array())
+    public function setControllerInfo($controllerInfo = array())
     {
-        $this->_moduleinfo = $moduleinfo;
+        $this->controllerInfo = $controllerInfo;
     }
 
     public function isStartPage()
@@ -165,34 +300,168 @@ class Application extends Base
         $this->_isStartPage = $bool;
     }
 
-    public function getmoduleinfo()
+    public function getControllerInfo()
     {
-        return $this->_moduleinfo;
+        return $this->controllerInfo;
     }
 
     /**
-     * Executes a module
+     * Get a controller
      * @param string $controller
+     * @return \Pramnos\Application\Controller
      */
-    public function exec($controller = '')
+    public function getController($controller)
     {
+        $className = ucfirst($controller);
+        $namespace = 'Pramnos';
+        if (isset($this->applicationInfo['namespace'])) {
+            $namespace = $this->applicationInfo['namespace'];
+        }
+        $nameSpacedClass = '\\' . $namespace . '\\Controllers\\' . $className;
+        if (class_exists($nameSpacedClass)) {
+            return new $nameSpacedClass();
+        }
+        $controllerObject = $this->getFrameworkController($controller);
+        if ($controllerObject) {
+            return $controllerObject;
+        }
 
+        throw new \Exception('No controller found: ' . $controller);
+    }
+
+
+
+    /**
+     * Executes a controller
+     * @param string $coontrollerName
+     */
+    public function exec($coontrollerName = '')
+    {
+        $controller = strtolower($coontrollerName);
+        if ($controller === '' && $this->controller === '') {
+            if ($this->defaultController !== "") {
+                $this->controller = $this->defaultController;
+            } else {
+                $this->close('There is no controller to run...');
+            }
+        } elseif ($controller != '') {
+            $this->controller = $controller;
+        }
+        if (\Pramnos\Application\Settings::getSetting('forcessl') == '1') {
+            if (strpos(sURL, 'https') !== 0) {
+                $this->redirect(
+                    str_replace('http://', 'https://', sURL), true, 301
+                );
+            }
+        }
+        $doc = \Pramnos\Framework\Factory::getDocument();
+
+
+
+        try {
+            $controllerObject = $this->getController($this->controller);
+        } catch (\Exception $Exception) {
+            \Pramnos\Logs\Logs::log($Exception->getMessage());
+            $this->close('There is no controller to run...');
+        }
+        $this->activeController = $controllerObject;
+        try {
+            $doc->addContent($controllerObject->exec($this->action));
+        } catch (Exception $exception) {
+            $message = $exception->getMessage();
+            if (strpbrk($message, 'SQL') !== false) {
+                \Pramnos\Logs\Logs::log(
+                    $message
+                    . "\nLine:\n"
+                    . $exception->getFile()
+                    . " -> "
+                    . $exception->getLine()
+                    . "\nTrace:\n"
+                    . $exception->getTraceAsString()
+                );
+            }
+            $this->redirect(URL);
+        }
+    }
+
+    /**
+     * Factory method
+     * @param string $app
+     * @return \Pramnos\Application\Application
+     * @throws Exception
+     */
+    public static function &getInstance($app = '')
+    {
+        if ($app == '' && self::$lastUsedApplication !== null) {
+            $app = self::$lastUsedApplication;
+        }
+        if ($app == '') {
+            $app = 'default';
+
+        }
+        if (!isset(self::$appInstances[$app])) {
+            if (!defined('DS')) {
+                define('DS', DIRECTORY_SEPARATOR);
+            }
+            if (!defined('APP_PATH')) {
+                define('APP_PATH', ROOT . DS . 'app');
+            }
+            try {
+                if ($app == 'default') {
+                    $tmpConfig = require APP_PATH . DS . 'app.php';
+                } else {
+                    $tmpConfig = require APP_PATH . DS . $app . '.php';
+                }
+
+                if (isset($tmpConfig['namespace'])) {
+                    $class = '\\' . $tmpConfig['namespace'] . '\\Application';
+                } else {
+                    $class = '\\Pramnos\\Application';
+                }
+                if (class_exists($class)) {
+                    if ($app == 'default') {
+                        self::$appInstances['default'] = new $class();
+                    } else {
+                        self::$appInstances['default'] = new $class($app);
+                    }
+                }
+
+            } catch (\Exception $Exception) {
+                \Pramnos\Logs\Logs::log(
+                    'Cannot start ' . $app . ' application: '
+                    . $Exception->getMessage()
+                );
+            }
+        }
+        return self::$appInstances[$app];
     }
 
     /**
      * This should be called for default pramnos_factory controllers when no
      * controller is found
-     * @param string $module
+     * @param string $controller
      * @return \pramnos_application_controller
      */
-    protected function getFrameworkController($module)
+    protected function getFrameworkController($controller)
     {
+        $className = ucfirst($controller);
+        $nameSpacedClass = '\\Pramnos\\Application\\Controllers\\' . $className;
+        if (class_exists($nameSpacedClass)) {
+            return $controller = new $nameSpacedClass();
+        }
 
+        return false;
     }
 
+    /**
+     * Render the application and return the content
+     * @return string
+     */
     public function render()
     {
-
+        $this->redirect(); //Redirect if it's needed
+        $doc = \Pramnos\Framework\Factory::getDocument();
+        return $doc->render();
     }
 
     /**
@@ -201,28 +470,15 @@ class Application extends Base
      */
     public function close($msg = "")
     {
-
+        if (defined('DEVELOPMENT') && DEVELOPMENT == true) {
+            \Pramnos\Logs\Logs::log(
+                \Pramnos\General\Helpers::varDumpToString(debug_backtrace()),
+                'exitAppLog'
+            );
+        }
+        exit($msg);
     }
 
-    /**
-     * Adds an extra path to the application to look for models and views
-     * @param string $path
-     * @return pramnos_application
-     */
-    public function _addExtraPath($path)
-    {
-        $this->_extraPath[] = $path;
-        return $this;
-    }
-
-    /**
-     * Return the array with all paths
-     * @return array
-     */
-    public function _getExtraPaths()
-    {
-        return $this->_extraPath;
-    }
 
 
 
