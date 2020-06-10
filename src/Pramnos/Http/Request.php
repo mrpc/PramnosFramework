8cbe1709c6834c2db201182f593802228fad02f6
mrpc <mrpc@pramnoshosting.gr>
2020-06-10 12:03:13 +0300

    Fixes in request for cli version

--- src/Pramnos/Http/Request.php
+++ src/Pramnos/Http/Request.php
@@ -384,9 +384,9 @@ class Request extends Base
                 && $_SERVER["SERVER_PORT"] != "80") {
                 $pageURL .= $_SERVER["SERVER_NAME"] . ":"
                     . $_SERVER["SERVER_PORT"] . $_SERVER["REQUEST_URI"];
-            } else {
+            } elseif (isset($_SERVER['SERVER_NAME'])) {
                 $pageURL .= $_SERVER["SERVER_NAME"] . $_SERVER["REQUEST_URI"];
-            }
+            } 
         } else {
             $pageURL = $_SERVER["REQUEST_URI"];
         }
