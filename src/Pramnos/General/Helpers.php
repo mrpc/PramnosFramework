d78b1e61ea81d51d25190994d7afdbce0c6a6507
mrpc <mrpc@pramnoshosting.gr>
2020-10-09 19:18:46 +0300

    Passing the encoding as 3rd parameter to mb_strrpos() is deprecated. Instead pass a 0 offset, and encoding as 4th parameter. & removal of truncate method

--- src/Pramnos/General/Helpers.php
+++ src/Pramnos/General/Helpers.php
@@ -760,10 +760,17 @@ class Helpers
         }
         $returnText = trim(strip_tags($text));
         if (mb_strlen($returnText, $charset) > $length) {
-            $lastSpace = mb_strrpos(
-                mb_substr($returnText, 0, $length, $charset),
-                ' ', $charset
-            );
+            if (version_compare(PHP_VERSION, '7.4.0') >= 0) {
+                $lastSpace = mb_strrpos(
+                    mb_substr($returnText, 0, $length, $charset),
+                    ' ', 0, $charset
+                );
+            } else {
+                $lastSpace = mb_strrpos(
+                    mb_substr($returnText, 0, $length, $charset),
+                    ' ', $charset
+                );
+            }
             $returnText = mb_substr($returnText, 0, $lastSpace, $charset)
                 . $moreText;
         }
@@ -771,177 +778,7 @@ class Helpers
     }
 
 
-    /**
-     * Truncates text.
-     *
-     * Cuts a string to the length of $length and replaces the last characters
-     * with the ellipsis if the text is longer than length.
-     *
-     * ### Options:
-     *
-     * - `ellipsis` Will be used as Ending and appended to the trimmed string
-     * (`ending` is deprecated)
-     * - `exact` If false, $text will not be cut mid-word
-     * - `html` If true, HTML tags would be handled correctly
-     *
-     * Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
-     *
-     * Licensed under The MIT License
-     * For full copyright and license information, please see the LICENSE.txt
-     * Redistributions of files must retain the above copyright notice.
-     *
-     * @copyright     Copyright (c) Cake Software Foundation, Inc.
-     *                (http://cakefoundation.org)
-     * @link          http://cakephp.org CakePHP(tm) Project
-     * @license       http://www.opensource.org/licenses/mit-license.php
-     *                MIT License
-     *
-     * @param string $text String to truncate.
-     * @param integer $length Length of returned string, including ellipsis.
-     * @param array $options An array of html attributes and options.
-     * @return string Trimmed string.
-     * @link http://book.cakephp.org/2.0/en/core-libraries/helpers/text.html
-     */
-    public static function truncate($text, $length = 100, $options = array(),
-        $charset = 'UTF-8')
-    {
-        $default = array(
-            'ellipsis' => '...', 'exact' => false, 'html' => true
-        );
-        if (isset($options['ending'])) {
-            $default['ellipsis'] = $options['ending'];
-        } elseif (!empty($options['html']) && $charset === 'UTF-8') {
-            $default['ellipsis'] = "\xe2\x80\xa6";
-        }
-        $options = array_merge($default, $options);
-        extract($options);
-
-        if (!function_exists('mb_strlen')) {
-            class_exists('Multibyte');
-        }
-
-        if ($html) {
-            if (mb_strlen(
-                preg_replace('/<.*?>/', '', $text), $charset
-            ) <= $length) {
-                return $text;
-            }
-            $totalLength = mb_strlen(strip_tags($ellipsis), $charset);
-            $openTags = array();
-            $truncate = '';
-
-            preg_match_all(
-                '/(<\/?([\w+]+)[^>]*>)?([^<>]*)/', $text, $tags,
-                PREG_SET_ORDER
-            );
-            foreach ($tags as $tag) {
-                if (!preg_match(
-                    '/img|br|input|hr|area|base|basefont|col|'
-                    . 'frame|isindex|link|meta|param/s', $tag[2]
-                )) {
-                    if (preg_match('/<[\w]+[^>]*>/s', $tag[0])) {
-                        array_unshift($openTags, $tag[2]);
-                    } elseif (preg_match(
-                        '/<\/([\w]+)[^>]*>/s', $tag[0],
-                        $closeTag
-                    )) {
-                        $pos = array_search($closeTag[1], $openTags);
-                        if ($pos !== false) {
-                            array_splice($openTags, $pos, 1);
-                        }
-                    }
-                }
-                $truncate .= $tag[1];
-
-                $contentLength = mb_strlen(
-                    preg_replace(
-                        '/&[0-9a-z]{2,8};|&#[0-9]{1,7};|&#x[0-9a-f]{1,6};/i',
-                        ' ', $tag[3]
-                    ), $charset
-                );
-                if ($contentLength + $totalLength > $length) {
-                    $left = $length - $totalLength;
-                    $entitiesLength = 0;
-                    if (preg_match_all(
-                        '/&[0-9a-z]{2,8};|&#[0-9]{1,7};|&#x[0-9a-f]{1,6};/i',
-                        $tag[3], $entities, PREG_OFFSET_CAPTURE
-                    )) {
-                        foreach ($entities[0] as $entity) {
-                            if ($entity[1] + 1 - $entitiesLength <= $left) {
-                                $left--;
-                                $entitiesLength += mb_strlen(
-                                    $entity[0], $charset
-                                );
-                            } else {
-                                break;
-                            }
-                        }
-                    }
-
-                    $truncate .= mb_substr(
-                        $tag[3], 0, $left + $entitiesLength, $charset
-                    );
-                    break;
-                } else {
-                    $truncate .= $tag[3];
-                    $totalLength += $contentLength;
-                }
-                if ($totalLength >= $length) {
-                    break;
-                }
-            }
-        } else {
-            if (mb_strlen($text, $charset) <= $length) {
-                return $text;
-            }
-            $truncate = mb_substr(
-                $text, 0, $length - mb_strlen($ellipsis, $charset), $charset
-            );
-        }
-        if (!$exact) {
-            $spacepos = mb_strrpos($truncate, ' ', $charset);
-            if ($html) {
-                $truncateCheck = mb_substr($truncate, 0, $spacepos, $charset);
-                $lastOpenTag = mb_strrpos($truncateCheck, '<', $charset);
-                $lastCloseTag = mb_strrpos($truncateCheck, '>', $charset);
-                if ($lastOpenTag > $lastCloseTag) {
-                    preg_match_all(
-                        '/<[\w]+[^>]*>/s', $truncate, $lastTagMatches
-                    );
-                    $lastTag = array_pop($lastTagMatches[0]);
-                    $spacepos = mb_strrpos($truncate, $lastTag, $charset)
-                        + mb_strlen($lastTag, $charset);
-                }
-                $bits = mb_substr($truncate, $spacepos, NULL, $charset);
-                preg_match_all(
-                    '/<\/([a-z]+)>/', $bits, $droppedTags, PREG_SET_ORDER
-                );
-                if (!empty($droppedTags)) {
-                    if (!empty($openTags)) {
-                        foreach ($droppedTags as $closingTag) {
-                            if (!in_array($closingTag[1], $openTags)) {
-                                array_unshift($openTags, $closingTag[1]);
-                            }
-                        }
-                    } else {
-                        foreach ($droppedTags as $closingTag) {
-                            $openTags[] = $closingTag[1];
-                        }
-                    }
-                }
-            }
-            $truncate = mb_substr($truncate, 0, $spacepos, $charset);
-        }
-        $truncate .= $ellipsis;
-
-        if ($html) {
-            foreach ($openTags as $tag) {
-                $truncate .= '</' . $tag . '>';
-            }
-        }
-
-        return $truncate;
-    }
+  
 
     /**
      * Optimize timestamps for use in sql or cache queries
