7a2b21cff8a3c5660fc721c0285ad82beba23d7b
mrpc <mrpc@pramnoshosting.gr>
2026-04-08 14:29:14 +0300

    Centralize validation and harden security infrastructure - Consolidated legacy validation methods into Pramnos\Validation\Validator. - Implemented session-stable CSRF protection with automatic validation rule. - Hardened cookie security flags (HttpOnly, SameSite, Secure). - Refactored internal framework calls (Request, Helpers) to use modern Validator. - Achieved 100% code coverage for Validation and ValidationException classes. - Restored and enhanced Pramnos_Validation_System_Guide.md documentation. - Maintained backward compatibility for Pramnos\General\Validator with deprecation notices.

--- src/Pramnos/General/Validator.php
+++ src/Pramnos/General/Validator.php
@@ -1,88 +1,76 @@
 <?php
+
 namespace Pramnos\General;
+
 /**
- * Includes all validation and filter functions
+ * Legacy Validator class, maintained for backward compatibility.
+ * Redirects all calls to the modern Validation system.
+ * 
  * @package     PramnosFramework
  * @subpackage  Validation
+ * @deprecated Use \Pramnos\Validation\Validator instead.
  */
-class Validator extends \Pramnos\Framework\Base
+class Validator extends \Pramnos\Validation\Validator
 {
+    /**
+     * @inheritdoc
+     */
+    public static function validate(
+        array $data,
+        array $rules,
+        array $messages = [],
+        array $attributes = []
+    ): array {
+        self::triggerDeprecation();
+        return parent::validate($data, $rules, $messages, $attributes);
+    }
 
     /**
-     * Check if an email address is valid and sanitize it
-     * @todo   PHP has a built in function called checkdnsrr() which will
-     *         take an  email address and check if it resolves as an IP
-     *         address. This is very  cool when sending emails for example.
-     *         Should checkdnsrr() return false  while you are trying to send
-     *         an email with this function, you can return  an error informing
-     *         the user that the domain probably doesn't exist  before you do
-     *         anything else.
-     * @param  string $email
-     * @return mixed  Returns the sanitized email address or FALSE for invalid
+     * Trigger a deprecation notice.
+     */
+    protected static function triggerDeprecation()
+    {
+        trigger_error(
+            'Pramnos\General\Validator is deprecated. Please use \Pramnos\Validation\Validator instead.', 
+            E_USER_DEPRECATED
+        );
+    }
+
+    /**
+     * @inheritdoc
      */
     public static function checkEmail($email)
     {
-        $emailFixed = filter_var(
-            strtolower(trim($email)), FILTER_SANITIZE_EMAIL
-        );
-
-        if (!filter_var($emailFixed, FILTER_VALIDATE_EMAIL)) {
-            return false;
-        } else {
-            return $emailFixed;
-        }
+        self::triggerDeprecation();
+        return parent::checkEmail($email);
     }
 
     /**
-     * Check if a string is a valid JSON
-     * @param string $string
-     * @return boolean true if the string is a valid JSON
+     * @inheritdoc
      */
-    public static function isJson($string)
+    public static function isJson($string): bool
     {
-        if (version_compare(PHP_VERSION, '8.3.0', '>=')) {
-            return json_validate($string);
-        }
-        json_decode($string);
-        return (json_last_error() === JSON_ERROR_NONE);
+        self::triggerDeprecation();
+        return parent::isJson($string);
     }
 
     /**
-     * Check if a link is valid
-     * @param string $url Url to parse
-     * @return boolean true if the url is valid
+     * @inheritdoc
      */
     public static function checkLink($url)
     {
-        $urlToCheck = trim($url);
-        $reg_exUrl = "/^(http|https|ftp|ftps)\:\/\/(.*)/i";
-        // Check if there is a url in the text
-        if (!preg_match($reg_exUrl, $urlToCheck)) {
-            $urlToCheck = "http://" . $urlToCheck;
-        }
-        $finalUrl = filter_var($urlToCheck, FILTER_SANITIZE_URL);
-        //FILTER_VALIDATE_URL allows url without dot
-        if (strpos($finalUrl, ".") == 0) {
-            return false;
-        }
-        if (!filter_var($finalUrl, FILTER_VALIDATE_URL)) {
-            return false;
-        }
-        return $finalUrl;
+        self::triggerDeprecation();
+        return parent::checkLink($url);
     }
 
     /**
-     * Factory function
-     * @staticvar Validator $instance
-     * @return Validator
+     * Factory function for legacy support.
+     *
+     * @return static
      */
     public static function &getInstance()
     {
-        static $instance;
-        if (!is_object($instance)) {
-            $instance = new Validator();
-        }
-        return $instance;
+        self::triggerDeprecation();
+        return parent::getInstance();
     }
-
 }
