60cf41f9635476f1ac4322e4f9be1d24d9833312
mrpc <mrpc@pramnoshosting.gr>
2026-07-17 11:30:46 +0300

    chore(scaffold): remove "Powered by" line from theme footers

--- scaffolding/themes/bootstrap/footer.php
+++ scaffolding/themes/bootstrap/footer.php
@@ -1,7 +1,6 @@
     <footer class="bg-dark text-light py-4 mt-auto">
         <div class="container text-center">
-            <p class="mb-1">&copy; <?php echo date('Y'); ?> <?php echo \Pramnos\Application\Application::getInstance()->applicationInfo['name']; ?>. All rights reserved.</p>
-            <p class="mb-0 text-muted small">Powered by <a href="https://github.com/mrpc/PramnosFramework" target="_blank" class="text-secondary">PramnosFramework</a></p>
+            <p class="mb-0">&copy; <?php echo date('Y'); ?> <?php echo \Pramnos\Application\Application::getInstance()->applicationInfo['name']; ?>. All rights reserved.</p>
         </div>
     </footer>
     <script src="<?php echo sURL; ?>assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
